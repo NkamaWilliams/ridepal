c735829e23c3160b309ed11fe927b5ae9bc0dea4
Williams <williamsnkama@gmail.com>
2024-08-10 05:31:14 +0100

    Change: Datetime field

--- src/components/form/date-time.tsx
+++ src/components/form/date-time.tsx
@@ -6,8 +6,9 @@ export default function DateTime(){
         <div className={styles.inputGroup}>
             <label htmlFor="date" className={styles.bold}>Date and time</label>
             <div className={styles.datetime}>
-                <input alt="Date" id="date" title="date" type="date" required/>
-                <input alt="Time" id="time" title="time" type="time" required/>
+                {/* <input onChange={(e) => {console.log(e.target.value)}} alt="Date" id="date" title="date" type="date" required/>
+                <input onChange={(e) => {console.log(e.target.value)}} alt="Time" id="time" title="time" type="time" required/> */}
+                <input type="datetime-local" id="datetime" title="datetime" name="datetime"  onChange={(e) => {console.log(e.target.value)}}/>
             </div>
         </div>
     )
